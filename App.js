f5e997efcde1b80a4f91a6a979caf3e61f2a55a0
ankitsdglobal <ankit@hatiintl.com>
2020-09-11 13:34:04 +0800

    first and final commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import 'react-native-gesture-handler';
+import {enableScreens} from 'react-native-screens';
+import React from 'react';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
+import authReducer from './store/reducers/auth';
+import NavigationContainer from './navigation/NavigationContainer';
+import ReduxThunk from 'redux-thunk';
+//import {composeWithDevTools} from 'redux-devtools-extension';
+
+enableScreens();
+const rootReducer = combineReducers({
+  products: productsReducer,
+  cart: cartReducer,
+  orders: ordersReducer,
+  auth: authReducer,
+});
+
+//const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer />
+    </Provider>
+  );
+}
